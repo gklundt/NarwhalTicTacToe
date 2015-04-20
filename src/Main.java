cad5e9b27f8d223b370a96a0da53e6ee7242d849
gklundt <gklundt@gmail.com>
2015-04-19 20:44:24 -0500

    Removing comment

--- src/Main.java
+++ src/Main.java
@@ -4,24 +4,20 @@
  * and open the template in the editor.
  */
 //this is a test pls ignore ;)
+
 /**
  *
  * @author aftonmartin
  */
 public class Main {
-	public static void main(String args[]){
-            final GameData data;
-            data = new GameData();
-            AbstractProtocolAdapter pa = new ProtocolAdapter();
-            pa.start("",data);
-            
-            System.out.printf("ID: %s \n"
-                    + "Mode: %s \n"
-                    + "Player: %s \n"
-                    + "Result: %s \n"
-                    + "Time: %s \n", data.gameId, data.gameMode, data.player, data.result, data.timeLeft);
-            
+
+    public static void main(String args[]) {
+        final GameData data;
+        data = new GameData();
+        AbstractProtocolAdapter pa = new ProtocolAdapter();
+        pa.start("", data);
+
 //            GameEngine ge = new Engine();
 //            GameWindow gw = new GameWindow(new GameController());
-	}	
+    }
 }
