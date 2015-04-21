168e9b21a10b0b1fa9096fb5eaedee58aee525d4
gklundt <gklundt@gmail.com>
2015-04-20 23:23:04 -0500

    Added getOpponentMove and updated flow Signed-off-by: gklundt <gklundt@gmail.com>

--- src/RestProtocolAdapterBehavior.java
+++ src/RestProtocolAdapterBehavior.java
@@ -12,6 +12,7 @@ public class RestProtocolAdapterBehavior implements ProtocolAdapterBehaviorInter
 
     private String playerId;
     private final String[] apiBoard;
+    private Player imPlayer;
 
     public RestProtocolAdapterBehavior() {
         this.apiBoard = new String[]{"", "", "", "", "", "", "", "", ""};
@@ -21,34 +22,68 @@ public class RestProtocolAdapterBehavior implements ProtocolAdapterBehaviorInter
     public void start(String uri, GameData data) {
 
         data.gameMode = GameMode.NORMAL;
-        // todo: set data.gameSequence
 
         if (data.gameId == null || data.gameId.length() == 0) {
             data.gameId = this.api_start();
             data.player = Player.PLAYER1;
+            this.imPlayer = Player.PLAYER1;
         } else {
             data.player = Player.PLAYER2;
+            this.imPlayer = Player.PLAYER2
         }
 
         data.result = Result.NONE;
         data.timeLeft = 30000;
         this.playerId = this.api_connect(data.gameId);
         System.out.printf("PlayerId: %s \n", this.playerId);
+        this.waitForMyTurn(data);
+    }
 
-        while (this.api_status(data.gameId) == 0) {
+    @Override
+    public void getOpponentMove(GameData data) {
+        // send my move
+        boolean accepted = this.api_move(data.gameId, this.playerId, data.gameSequence.getLast());
+        if (accepted) {
+            this.waitForMyTurn(data);
+            data.player = data.player == Player.PLAYER1 ? Player.PLAYER2 : Player.PLAYER1;
+        }
+        // not accepted data continues unchanged
+    }
+
+    private void waitForMyTurn(GameData data) {
+
+        int gamestatus = this.api_status(data.gameId);
+        int myTurn = this.imPlayer == Player.PLAYER1 ? 1 : 2;
+        int retries = 30;
+
+        while (gamestatus != myTurn && gamestatus < 3 && retries > 0) {
             try {
+                gamestatus = this.api_status(data.gameId);
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
                 Logger.getLogger(RestProtocolAdapterBehavior.class.getName()).log(Level.SEVERE, null, ex);
             }
+            --retries;
         }
 
-    }
+        switch (gamestatus) {
+            case 1:
+                this.api_grid(data.gameId, data);
+                break;
+            case 2:
+                this.api_grid(data.gameId, data);
+                break;
+            case 3:
+                data.result = this.imPlayer = Player.PLAYER1 ? Result.WIN : Result.LOSE;
+                break;
+            case 4:
+                data.result = this.imPlayer = Player.PLAYER2 ? Result.WIN : Result.LOSE;
+                break;
+            default:
+                // dont' know ... we win!
+                data.result = Result.WIN;
+        }
 
-    @Override
-    public void getOpponentMove(GameData data
-    ) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     private String api_start() {
@@ -168,10 +203,9 @@ public class RestProtocolAdapterBehavior implements ProtocolAdapterBehaviorInter
         } catch (IOException ex) {
             Logger.getLogger(ProtocolAdapter.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         //API returns ...
         //["X","O","O","","X","X","","","O"]
-
         String[] s = content.toString().split(",");
         for (int i = 0; i < s.length; ++i) {
             s[i] = s[i].replace("[", "").replace("\"", "").replace("]", "").toUpperCase();
