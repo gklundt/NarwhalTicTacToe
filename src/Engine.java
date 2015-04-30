b5d3339de6352fef40652fb4c043bea2f6b3e2f0
NoahSefcik <sefcik.noah@gmail.com>
2015-04-30 03:23:36 -0500

    Update

--- src/Engine.java
+++ src/Engine.java
@@ -34,34 +34,34 @@ public class Engine extends GameEngine {
         if (game.gameSequence.isEmpty()) {
             //This block indicates first move of game.
             myNextMove = gb.getMoveCommon(enemyMoves, movesLeft, myMoves);
-            myMoves.add(myNextMove);
+            myMoves.add(Integer.valueOf(myNextMove));
             movesLeft.remove(Integer.valueOf(myNextMove));
         } else if (game.gameSequence.size() == 8) {
             //This block indicates first move of ACHI.
             gb = new AchiBehavior();
             enemyLastMove = game.gameSequence.getLast();
-            enemyMoves.add(enemyLastMove);
+            enemyMoves.add(Integer.valueOf(enemyLastMove));
             movesLeft.remove(Integer.valueOf(enemyLastMove));
             myNextMove = gb.getMoveCommon(enemyMoves, movesLeft, myMoves);
-            myMoves.add(myNextMove);
-            movesLeft.set(0, myNextMove);
+            myMoves.add(Integer.valueOf(myNextMove));
+            movesLeft.set(0, Integer.valueOf(myNextMove));
         } else if (game.gameSequence.size() > 8) {
             //This block indicates full ACHI mode.
             gb = new AchiBehavior();
             enemyMoves.remove(Integer.valueOf(game.gameSequence.getLast()));
-            enemyMoves.add(movesLeft.get(movesLeft.size() - 1));
-            movesLeft.set(0, game.gameSequence.getLast());
+            enemyMoves.add(Integer.valueOf(movesLeft.get(movesLeft.size() - 1)));
+            movesLeft.set(0, Integer.valueOf(game.gameSequence.getLast()));
             myNextMove = gb.getMoveCommon(enemyMoves, movesLeft, myMoves);
             myMoves.remove(Integer.valueOf(myNextMove));
-            myMoves.add(movesLeft.get(movesLeft.size() - 1));
-            movesLeft.set(0, myNextMove);
+            myMoves.add(Integer.valueOf(movesLeft.get(movesLeft.size() - 1)));
+            movesLeft.set(0, Integer.valueOf(myNextMove));
         } else {
             //This block indicates normal mode.
             enemyLastMove = game.gameSequence.getLast();
-            enemyMoves.add(enemyLastMove);
+            enemyMoves.add(Integer.valueOf(enemyLastMove));
             movesLeft.remove(Integer.valueOf(enemyLastMove));
             myNextMove = gb.getMoveCommon(enemyMoves, movesLeft, myMoves);
-            myMoves.add(myNextMove);
+            myMoves.add(Integer.valueOf(myNextMove));
             movesLeft.remove(Integer.valueOf(myNextMove));
         }
         //Debugging code comment block
--- src/TicTacToeBehavior.java
+++ src/TicTacToeBehavior.java
@@ -6,15 +6,13 @@ import java.util.*;
  */
 public class TicTacToeBehavior implements GameBehavior {
 
-    int move;
     int[] sidePositions = {1, 3, 5, 7};
     int[] cornerPositions = {0, 2, 6, 8};
 
     public TicTacToeBehavior() {
-        move = 1;
     }
 
-    private boolean detectCornerMove() {
+    private boolean detectCornerMove(int move) {
         for (int i = 0; i < cornerPositions.length; i++) {
             if (cornerPositions[i] == move) {
                 return true;
@@ -42,31 +40,41 @@ public class TicTacToeBehavior implements GameBehavior {
     @Override
     public int getMoveCommon(ArrayList<Integer> enemyMoves, ArrayList<Integer> movesLeft, ArrayList<Integer> myMoves) {
         int myNextMove = 0;
-        if (enemyMoves.isEmpty()) {
-            myNextMove = 4;
-        } else {
-            switch (move) {
-                case 2:
-                    if (enemyMoves.contains(4)) {
-                        myNextMove = 0;
-                    }
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-                case 6:
-                    break;
-                case 7:
-                    break;
-                default:
-                    break;
-            }
+        switch (9 - movesLeft.size()) {
+            case 0:
+                myNextMove = 4;
+                break;
+            case 1:
+                if (enemyMoves.contains(Integer.valueOf(4))) {
+                    myNextMove = 0;
+                } else {
+                    myNextMove = 4;
+                }
+                break;
+            case 2:
+                if (detectCornerMove(enemyMoves.get(enemyMoves.size() - 1))) {
+                    
+                } else if (detectSideMove(enemyMoves.get(enemyMoves.size() - 1))) {
+                    
+                }
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            default:
+                break;
         }
-        move++;
-        return myNextMove;
     }
+    move
+    ++;
+    return myNextMove ;
+}
 
 }
