d24ed1ff6447aeae453f402c610c060b72c11853
gklundt <gklundt@gmail.com>
2015-05-07 23:32:57 -0500

    Update ProtocolAdapterTest.java

--- test/ProtocolAdapterTest.java
+++ test/ProtocolAdapterTest.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -16,22 +10,22 @@ import static org.junit.Assert.*;
  * @author gordon
  */
 public class ProtocolAdapterTest {
-    
+
     public ProtocolAdapterTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -40,27 +34,75 @@ public class ProtocolAdapterTest {
      * Test of start method, of class ProtocolAdapter.
      */
     @Test
-    public void testStart() {
-        System.out.println("start");
+    public void testStart_Player1() {
+
+        System.out.println("start null game data");
         String uri = "";
-        GameData data = null;
         ProtocolAdapter instance = new ProtocolAdapter();
-        instance.start(uri, data);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        GameData data1 = new GameData();
+        instance.start(uri, data1);
+        assertTrue(data1.player == Player.PLAYER1);
+        assertNotNull(data1.gameId);
+        instance = null;
+        data1 = null;
+        System.out.println("end null game data");
+
+    }
+
+    @Test
+    public void testStart_Player2() {
+
+        System.out.println("start not null game data");
+        String uri = "";
+
+        ProtocolAdapter instance1 = new ProtocolAdapter();
+
+        GameData data1 = new GameData();
+        instance1.start(uri, data1);
+
+        instance1.getOpponentMove(data1);
+        assertTrue(data1.player == Player.PLAYER1);
+        assertNotNull(data1.gameId);
+
+        GameData data2 = new GameData();
+        data2.gameId = data1.gameId;
+
+        assertTrue(data1.gameId == data2.gameId);
+
+        ProtocolAdapter instance2 = new ProtocolAdapter();
+        instance2.start(uri, data2);
+        assertTrue(data2.player == Player.PLAYER2);
+
     }
 
     /**
      * Test of getOpponentMove method, of class ProtocolAdapter.
      */
     @Test
-    public void testGetOpponentMove() {
+    public void testGetOpponentMove_accepted() {
         System.out.println("getOpponentMove");
-        GameData data = null;
+        GameData data = new GameData();
+        data.gameSequence.add(4);
+        
         ProtocolAdapter instance = new ProtocolAdapter();
         instance.getOpponentMove(data);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        boolean pass = true;
+        assertTrue("Unable to test", pass);
     }
-    
+
+        /**
+     * Test of getOpponentMove method, of class ProtocolAdapter.
+     */
+    @Test
+    public void testGetOpponentMove_rejected() {
+        System.out.println("getOpponentMove");
+        GameData data = new GameData();
+        data.gameSequence.add(15);
+        
+        ProtocolAdapter instance = new ProtocolAdapter();
+        instance.getOpponentMove(data);
+        boolean pass = true;
+        assertTrue("Unable to test", pass);
+    }
+
 }
